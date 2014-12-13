163b39a1ff415021d63e5b0c11e10f5d6dfa939d
akkie <ck-github@mohiva.com>
2014-12-13 08:24:33 +0100

    Show error message if user already exists

--- app/controllers/SignUpController.scala
+++ app/controllers/SignUpController.scala
@@ -11,6 +11,7 @@ import com.mohiva.play.silhouette.impl.providers._
 import forms.SignUpForm
 import models.User
 import models.services.UserService
+import play.api.i18n.Messages
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc.Action
 
@@ -42,28 +43,33 @@ class SignUpController @Inject() (
       form => Future.successful(BadRequest(views.html.signUp(form))),
       data => {
         val loginInfo = LoginInfo(CredentialsProvider.ID, data.email)
-        val authInfo = passwordHasher.hash(data.password)
-        val user = User(
-          userID = UUID.randomUUID(),
-          loginInfo = loginInfo,
-          firstName = Some(data.firstName),
-          lastName = Some(data.lastName),
-          fullName = Some(data.firstName + " " + data.lastName),
-          email = Some(data.email),
-          avatarURL = None
-        )
-        for {
-          avatar <- avatarService.retrieveURL(data.email)
-          user <- userService.save(user.copy(avatarURL = avatar))
-          authInfo <- authInfoService.save(loginInfo, authInfo)
-          authenticator <- env.authenticatorService.create(user.loginInfo)
-          result <- env.authenticatorService.init(authenticator, Future.successful(
-            Redirect(routes.ApplicationController.index)
-          ))
-        } yield  {
-          env.eventBus.publish(SignUpEvent(user, request, request2lang))
-          env.eventBus.publish(LoginEvent(user, request, request2lang))
-          result
+        userService.retrieve(loginInfo).flatMap {
+          case Some(user) =>
+            Future.successful(Redirect(routes.ApplicationController.signUp()).flashing("error" -> Messages("user.exists")))
+          case None =>
+            val authInfo = passwordHasher.hash(data.password)
+            val user = User(
+              userID = UUID.randomUUID(),
+              loginInfo = loginInfo,
+              firstName = Some(data.firstName),
+              lastName = Some(data.lastName),
+              fullName = Some(data.firstName + " " + data.lastName),
+              email = Some(data.email),
+              avatarURL = None
+            )
+            for {
+              avatar <- avatarService.retrieveURL(data.email)
+              user <- userService.save(user.copy(avatarURL = avatar))
+              authInfo <- authInfoService.save(loginInfo, authInfo)
+              authenticator <- env.authenticatorService.create(user.loginInfo)
+              result <- env.authenticatorService.init(authenticator, Future.successful(
+                Redirect(routes.ApplicationController.index)
+              ))
+            } yield  {
+              env.eventBus.publish(SignUpEvent(user, request, request2lang))
+              env.eventBus.publish(LoginEvent(user, request, request2lang))
+              result
+            }
         }
       }
     )
