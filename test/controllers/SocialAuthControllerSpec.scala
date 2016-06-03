5dab9b6c3bb7ce863844d0c24c4618dd1c78fe77
Nick Shaw <nick.shaw@g4me.co.uk>
2016-06-03 15:28:50 +0100

    adding spec

---
+++ test/controllers/SocialAuthControllerSpec.scala
@@ -0,0 +1,23 @@
+package controllers
+
+import java.util.UUID
+
+import com.google.inject.AbstractModule
+import com.mohiva.play.silhouette.api.{ Environment, LoginInfo }
+import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
+import com.mohiva.play.silhouette.test._
+import models.User
+import net.codingwell.scalaguice.ScalaModule
+import org.specs2.mock.Mockito
+import org.specs2.specification.Scope
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.test.{ FakeRequest, PlaySpecification, WithApplication }
+import utils.auth.DefaultEnv
+
+/**
+ * Test case for the [[controllers.SocialAuthController]] class.
+ */
+class SocialAuthControllerSpec extends PlaySpecification with Mockito {
+  sequential
+}
