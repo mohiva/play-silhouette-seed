d6e5efaf66a0bfd66cb26f89a7c3f6e4665d4b70
bravegag <bravegag@gmail.com>
2019-06-12 10:40:10 +0200

    Adapting new joda routes mapping changes to two different date-time formats

--- app/utils/route/MyJodaRoutes.scala
+++ app/utils/route/MyJodaRoutes.scala
@@ -2,6 +2,7 @@ package utils.route
 
 object MyJodaRoutes {
   val myJodaRoutes = new com.github.tototoshi.play2.routes.JodaRoutes {
-    override val format: String = "dd-MM-yyyy"
+    override val localDateFormat: String = "dd-MM-yyyy"
+    override val dateTimeFormat: String = "dd-MM-yyyyTHH:mm:ss"
   }
 }
