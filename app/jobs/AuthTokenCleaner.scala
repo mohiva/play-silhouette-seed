97d6876636d9323e34e661ab44bb824acee78e04
Will Sargent <will.sargent@gmail.com>
2019-12-29 11:31:58 -0800

    Remove Logger trait, use play.api.Logging (#117)

--- app/jobs/AuthTokenCleaner.scala
+++ app/jobs/AuthTokenCleaner.scala
@@ -1,12 +1,11 @@
 package jobs
 
 import javax.inject.Inject
-
 import akka.actor._
 import com.mohiva.play.silhouette.api.util.Clock
 import jobs.AuthTokenCleaner.Clean
 import models.services.AuthTokenService
-import utils.Logger
+import play.api.Logging
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -19,7 +18,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class AuthTokenCleaner @Inject() (
   service: AuthTokenService,
   clock: Clock)
-  extends Actor with Logger {
+  extends Actor with Logging {
 
   /**
    * Process the received messages.
